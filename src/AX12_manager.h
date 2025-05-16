157f09dbf109b43e52f1f4ce09985eaf03160d3c
Victor Dubois <victor.dubois@navya.tech>
2025-05-16 20:39:26 +0200

    fix AX12

--- src/AX12_manager.h
+++ src/AX12_manager.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <Dynamixel.h> //https://github.com/hideakitai/Dynamixel
+// #include <Dynamixel.h> //https://github.com/hideakitai/Dynamixel => does not seem to work
+#include <Dynamixel2Arduino.h>
+#include "ESP32SerialPortHandler.cpp"
+
 #include "CanStruct/can_structs.h"
 
 #define DYNAMIXEL_SERIAL Serial2 // change as you want
@@ -8,9 +11,13 @@
 #define DYNAMIXEL_SERIAL_RX_pin 18
 #define NB_AX12 4
 const uint8_t PIN_RTS = 21;
-const uint32_t DYNAMIXEL_BAUDRATE = 1000000;
+// const uint32_t DYNAMIXEL_BAUDRATE = 1000000;
+const uint32_t DYNAMIXEL_BAUDRATE = 57600;
 
-Dynamixel dxl(PIN_RTS);
+Dynamixel2Arduino dxl;
+ESP32SerialPortHandler esp_dxl_port(DYNAMIXEL_SERIAL, DYNAMIXEL_SERIAL_RX_pin, DYNAMIXEL_SERIAL_TX_pin, PIN_RTS);
+
+// Dynamixel dxl(PIN_RTS);
 
 struct AX12
 {
@@ -39,60 +46,59 @@ struct AX12
 
 AX12 myAX12s[NB_AX12 + 2] = {AX12(1), AX12(2), AX12(3), AX12(4), AX12(5), AX12(6)};
 
-using namespace arduino;
-
 void setupDynamixel()
 {
-    DYNAMIXEL_SERIAL.begin(DYNAMIXEL_BAUDRATE, SERIAL_8N1, DYNAMIXEL_SERIAL_RX_pin, DYNAMIXEL_SERIAL_TX_pin);
-    dxl.attach(DYNAMIXEL_SERIAL, DYNAMIXEL_BAUDRATE);
+    pinMode(DYNAMIXEL_SERIAL_TX_pin, OUTPUT);
+    pinMode(DYNAMIXEL_SERIAL_RX_pin, INPUT);
+    pinMode(PIN_RTS, OUTPUT);
 
-    delay(1000);
+    digitalWrite(PIN_RTS, HIGH);
 
-    for (int i = 0; i < NB_AX12; i++)
-    {
-        Serial.println(i);
-        Serial.printf("Add Dynamixel n° %d with ID %d\n\r", i, myAX12s[i].id);
-        dxl.addModel<DxlModel::OTHER>(myAX12s[i].id);
-    }
+    dxl.setPort(esp_dxl_port);
+    dxl.setPortProtocolVersion(1);
+    dxl.begin(DYNAMIXEL_BAUDRATE);
 }
 
 void updateDynamixel(CAN::AX12Write a_ax12_msg, uint8_t ax12_id)
 {
     // a_ax12_msg.mode == // by default, they are all in position mode
 
-    dxl.goalPosition(ax12_id, a_ax12_msg.position);
-    dxl.torqueEnable(ax12_id, a_ax12_msg.torque_enable);
+    dxl.setGoalPosition(ax12_id, a_ax12_msg.position);
+    if (a_ax12_msg.torque_enable)
+    {
+        dxl.torqueOn(ax12_id);
+    }
+    else
+    {
+        dxl.torqueOff(ax12_id);
+    }
     if (a_ax12_msg.currentLimit != -1)
     {
-        dxl.currentLimit(ax12_id, a_ax12_msg.currentLimit);
+        dxl.setGoalCurrent(ax12_id, a_ax12_msg.currentLimit * 4);
     }
     if (a_ax12_msg.max_speed != -1)
     {
-        dxl.velocityLimit(ax12_id, a_ax12_msg.max_speed);
+        dxl.setGoalVelocity(ax12_id, a_ax12_msg.max_speed);
     }
     if (a_ax12_msg.temperatureLimit != -1)
     {
-        dxl.temperatureLimit(ax12_id, a_ax12_msg.temperatureLimit);
+        // dxl.setTemperatureLimit(ax12_id, a_ax12_msg.temperatureLimit);
     }
     if (a_ax12_msg.max_accel != -1)
     {
-        dxl.accelerationLimit(ax12_id, a_ax12_msg.max_accel);
+        // dxl.setAccelerationLimit(ax12_id, a_ax12_msg.max_accel);
     }
-    // dxl.velocityLimit(ax12_id, a_ax12_msg.max_speed);
-    // dxl.accelerationLimit(ax12_id, a_ax12_msg.max_accel);
-    // dxl.currentLimit(ax12_id, a_ax12_msg.currentLimit);
-    // dxl.temperatureLimit(ax12_id, a_ax12_msg.temperatureLimit);
 }
 
 void updateDynamixelInfo(CAN::AX12Read &a_ax12_msg, uint8_t ax12_id)
 {
     // a_ax12_msg.mode == // by default, they are all in position mode
 
-    a_ax12_msg.current_position = dxl.presentPosition(ax12_id);
-    a_ax12_msg.presentCurrent = dxl.presentCurrent(ax12_id);
-    a_ax12_msg.presentTemperature = dxl.presentTemperature(ax12_id);
-    a_ax12_msg.hardwareErrorStatus = dxl.hardwareErrorStatus(ax12_id);
-    a_ax12_msg.moving = dxl.movingStatus(ax12_id);
+    a_ax12_msg.current_position = dxl.getPresentPosition(ax12_id);
+    a_ax12_msg.presentCurrent = dxl.getPresentCurrent(ax12_id);
+    a_ax12_msg.presentTemperature = -1; // dxl.getPresentTemperature(ax12_id);
+    a_ax12_msg.hardwareErrorStatus = 0; // dxl.hardwareErrorStatus(ax12_id);
+    a_ax12_msg.moving = dxl.getPresentVelocity(ax12_id);
     a_ax12_msg.mode = 1; // dxl.mode(ax12_id);
 }
 
