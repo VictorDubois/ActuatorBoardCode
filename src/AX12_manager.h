dcc0f6ae6bcd8206df953e01257e0ecb42d70129
Victor Dubois <victor.dubois@navya.tech>
2026-04-26 20:03:34 +0200

    set 4th AX12 ID

--- src/AX12_manager.h
+++ src/AX12_manager.h
@@ -58,7 +58,7 @@ struct AX12
     }
 };
 
-AX12 myAX12s[NB_AX12 + 2] = {AX12(3), AX12(4), AX12(7), AX12(20), AX12(21), AX12(22)};
+AX12 myAX12s[NB_AX12 + 2] = {AX12(3), AX12(4), AX12(7), AX12(6), AX12(21), AX12(22)};
 
 void setupDynamixel()
 {
